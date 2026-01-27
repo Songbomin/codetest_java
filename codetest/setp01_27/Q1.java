34f074f8b82c756f14a8c78f9bedbfb0051b452c
Songbomin <bmsong00@naver.com>
2026-01-27 16:49:57 +0900

    01.27 문제 품.

---
+++ codetest/setp01_27/Q1.java
@@ -0,0 +1,52 @@
+package setp01_27;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1 {
+
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        selected = new int[M];
+
+        dfs(0);
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]);
+                if (i + 1 < M) sb.append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int num = 1; num <= N; num++) {
+            if (visited[num]) continue;
+
+            visited[num] = true;
+            selected[depth] = num;
+
+            dfs(depth + 1);
+
+            visited[num] = false;
+        }
+    }
+}
+
